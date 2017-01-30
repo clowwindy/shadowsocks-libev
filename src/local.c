9c7add71dd12e01ebd6c7564ce16db180ae09ea3
Max Lv <max.c.lv@gmail.com>
2017-01-30 08:41:04 +0800

    Fix #1148

--- src/local.c
+++ src/local.c
@@ -667,9 +667,13 @@ server_recv_cb(EV_P_ ev_io *w, int revents)
                         else if (atyp == 4)
                             LOGI("bypass [%s]:%s", ip, port);
                     }
+                    int err;
                     struct sockaddr_storage storage;
                     memset(&storage, 0, sizeof(struct sockaddr_storage));
-                    int err = get_sockaddr(ip, port, &storage, 0, ipv6first);
+                    if (sni_detected || atyp == 3)
+                       err = get_sockaddr(host, port, &storage, 0, ipv6first);
+                    else
+                       err = get_sockaddr(ip, port, &storage, 0, ipv6first);
                     if (err != -1) {
                         remote = create_remote(server->listener, (struct sockaddr *)&storage);
                         if (remote != NULL)
