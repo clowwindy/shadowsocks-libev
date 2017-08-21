feeaff85e156e09f1636ed3a47ac9375c81ceb87
Yuxiang Zhu <vfreex@gmail.com>
2017-08-21 11:03:08 +0800

    Provides separated service files for RPM packages
    As discussed with @librehat in #1604, maintaining a separate set of systemd
    files for RPM distributions will be convenient to build on Copr.
    
    This patch will also change some lines in rpm/genrpm.sh to archive the
    source code using `git archive` because the old logic doesn't respect
    `.gitignore`.

--- rpm/genrpm.sh
+++ rpm/genrpm.sh
@@ -34,10 +34,10 @@ do
 done
 
 # determine version and release number
-GIT_DESCRIBE=$(git describe --tags --match 'v*' --long --dirty)
+GIT_DESCRIBE=$(git describe --tags --match 'v*' --long)
 # GIT_DESCRIBE is like v3.0.3-11-g1e3f35c-dirty
 
-if [[ ! "$GIT_DESCRIBE" =~ ^v([^-]+)-([0-9]+)-g([0-9a-f]+)(-dirty)?$ ]]; then
+if [[ ! "$GIT_DESCRIBE" =~ ^v([^-]+)-([0-9]+)-g([0-9a-f]+)$ ]]; then
     >&2 echo 'ERROR - unrecognized `git describe` output: '"$GIT_DESCRIBE"
     exit 1
 fi
@@ -45,30 +45,31 @@ fi
 TARGET_VERSION=${BASH_REMATCH[1]}
 TARGET_COMMITS=${BASH_REMATCH[2]}
 TARGET_SHA1=${BASH_REMATCH[3]}
-TARGET_DIRTY=${BASH_REMATCH[4]}
 
 TARGET_RELEASE=1
 if [ "$TARGET_COMMITS" -gt 0 ]; then
     TARGET_RELEASE+=".$TARGET_COMMITS.git$TARGET_SHA1"
 fi
-if [ -n "$TARGET_DIRTY" ]; then
-    TARGET_RELEASE+=.dirty
-fi
 
 TARGET_VERREL=$TARGET_VERSION-$TARGET_RELEASE
 >&2 echo "INFO - RPM version-release is $TARGET_VERREL."
 
-
 # archive tarball from Git workspace
-TARGET_TARBALL_NAME=shadowsocks-libev-$TARGET_VERSION
-TARGET_TARBALL_DIR=$HERE/SOURCES
+export TARGET_TARBALL_NAME=shadowsocks-libev-$TARGET_VERSION
+export TARGET_TARBALL_DIR=$HERE/SOURCES
 mkdir -p -- "$TARGET_TARBALL_DIR"
-#git archive HEAD --format=tar --prefix="$TARGET_TARBALL_NAME/" \
-#    -o "$TARGET_TARBALL_DIR/$TARGET_TARBALL_NAME.tar"   
 pushd "$HERE"/..
-make clean
-tar --exclude './rpm' --exclude '.[^/]*'  --transform "s,^\.,$TARGET_TARBALL_NAME," \
-    -cvf "$TARGET_TARBALL_DIR/$TARGET_TARBALL_NAME.tar" .
+# archive this repo
+git archive HEAD --format=tar --prefix="$TARGET_TARBALL_NAME/" \
+    -o "$TARGET_TARBALL_DIR/$TARGET_TARBALL_NAME.tar"
+# archive submodules
+git submodule update --init
+git submodule foreach 'git archive HEAD --format=tar \
+        --prefix="$TARGET_TARBALL_NAME/$path/" \
+        -o "$TARGET_TARBALL_DIR/$TARGET_TARBALL_NAME-submodule-$path-$sha1.tar"
+    tar -n --concatenate --file="$TARGET_TARBALL_DIR/$TARGET_TARBALL_NAME.tar" \
+        "$TARGET_TARBALL_DIR/$TARGET_TARBALL_NAME-submodule-$path-$sha1.tar"'
+gzip -c "$TARGET_TARBALL_DIR/$TARGET_TARBALL_NAME.tar" > "$TARGET_TARBALL_DIR/$TARGET_TARBALL_NAME.tar.gz"
 popd
 
 # generate spec file
@@ -77,7 +78,7 @@ mkdir -p -- "$TARGET_SPEC_DIR"
 TARGET_SPEC_PATH=$TARGET_SPEC_DIR/shadowsocks-libev.spec
 sed -e "s/^\(Version:\).*$/\1       ${TARGET_VERSION}/" \
     -e "s/^\(Release:\).*$/\1       ${TARGET_RELEASE}%{?dist}/" \
-    -e "s/^\(Source0:\).*$/\1       ${TARGET_TARBALL_NAME}.tar/" \
+    -e "s/^\(Source0:\).*$/\1       ${TARGET_TARBALL_NAME}.tar.gz/" \
     "${TARGET_SPEC_PATH}".in > "${TARGET_SPEC_PATH}"
 
 # build rpms
