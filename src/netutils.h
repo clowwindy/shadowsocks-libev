38e83d6b5d4648fb87ae6680c8fa159df54c73ce
Max Lv <max.c.lv@gmail.com>
2019-04-30 09:34:31 +0800

    Make sure socket buffer size no larger than max chunk size

--- src/netutils.h
+++ src/netutils.h
@@ -55,7 +55,7 @@
 #define MAX_HOSTNAME_LEN 256 // FQCN <= 255 characters
 #define MAX_PORT_STR_LEN 6   // PORT < 65536
 
-#define SOCKET_BUF_SIZE (16 * 1024) // 16KB
+#define SOCKET_BUF_SIZE (16 * 1024 - 1) // 16383 Byte, equals to the max chunk size
 
 typedef struct {
     char *host;
