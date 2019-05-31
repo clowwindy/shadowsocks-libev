5e65dc0fb16a04024838f37663e58b55fe679a4b
papadave66 <papadavecn@gmail.com>
2019-05-31 16:33:00 +0800

    change from bzero(3) to memset(3) for better compatibility. (#2383)
    * change from bzero(3) to memset(3) for better compatibility.
    
    * cleanup

--- src/plugin.c
+++ src/plugin.c
@@ -308,7 +308,7 @@ get_local_port()
     }
 
     struct sockaddr_in serv_addr;
-    bzero((char *)&serv_addr, sizeof(serv_addr));
+    memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family      = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port        = 0;
