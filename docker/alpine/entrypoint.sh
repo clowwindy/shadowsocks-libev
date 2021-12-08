e21f8248b64528b9a30a87331074a0eae6676225
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2021-12-08 20:31:00 +0100

    fix: being able to use empty DNS_ADDRS with docker image

--- docker/alpine/entrypoint.sh
+++ docker/alpine/entrypoint.sh
@@ -8,12 +8,15 @@ if [[ -f "/var/run/secrets/$PASSWORD_SECRET" ]]; then
     PASSWORD=$(cat "/var/run/secrets/$PASSWORD_SECRET")
 fi
 
+if [[ ! -z "$DNS_ADDRS" ]]; then
+    ARGS="-d $DNS_ADDRS $ARGS"
+fi
+
 exec ss-server \
       -s $SERVER_ADDR \
       -p $SERVER_PORT \
       -k ${PASSWORD:-$(hostname)} \
       -m $METHOD \
       -t $TIMEOUT \
-      -d $DNS_ADDRS \
       -u \
       $ARGS
